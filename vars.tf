4b4229c64c78a0e932ecfec8ce6f5cbc97b11c36
demxtsk5 <82810367+demxtsk5@users.noreply.github.com>
2021-05-14 09:32:07 +0200

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,35 @@
+# Variablen
+variable "region" {
+  default = "eu-central-1"
+}
+variable "zone1" {
+  description = "Zone 1"
+  default = "eu-central-1a"
+}
+variable "cidr" {
+  description = "Main CIDR"
+  default = "10.0.0.0/21"  
+}
+variable "pub_net" {
+  description = "Public Network"
+  default = "10.0.1.0/24"  
+}
+variable "pri_net" {
+  description = "Private Network"
+  default = "10.0.2.0/24"  
+}
+variable "ami_ident" {
+  description = "AMI Identifier"
+  default = "ami-043097594a7df80ec"  
+}
+variable "ami_type" {
+  description = "AMI Type"
+  default = "t2.micro"
+}
+variable "my_tag" {
+  type = string
+  default = "mg0050"
+}
+variable "lb_name" {
+  default = "mg0050-lb"
+}
